0f482d5f183864bfca80554f74551e9eaa04994f
ES-CH <stromcaceres4@gmail.com>
2024-07-11 11:56:24 -0500

    chore: Add project information to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import { lusitana } from "../ui/fonts"
 
 export default async function DashboardPage() {
-
     return(
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
+            <p className="text-base md:text-lg">
+                Este proyecto fue creado utilizando <strong>Next.js</strong>, <strong>Axios</strong> para las solicitudes HTTP, y <strong>Tailwind CSS</strong> para el diseño.
+            </p>
         </main>
     )
 }
